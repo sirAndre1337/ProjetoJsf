e9b05da29ca1df862080609d73715bbf38f1e551
Andre Luis <andrelluis17@hotmail.com>
2021-09-13 17:42:57 -0300

    Classes responsaveis por toda a parte de mensagem do JSF

---
+++ src/main/java/br/com/project/util/all/Constante.java
@@ -0,0 +1,17 @@
+package br.com.project.util.all;
+
+import java.io.Serializable;
+
+/**
+ * Responsavel por carrega as mensagems do sistema
+ * v : 16
+ * @author André Luis
+ *
+ */
+public class Constante implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	public static final String SUCESSO = "sucesso";
+	public static final String OPERACAO_REALIZADA_COM_SUCESSO = "Operacao realizada com Sucesso.";
+	public static final String ERRO_NA_OPERACAO = "Não foi Possível Executar a Operação";
+}
